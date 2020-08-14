401af99a44e369846de7e1834c82344cbed110ae
Dhruvester <69696113+Dhruvester@users.noreply.github.com>
2020-08-14 13:13:15 -0700

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("I am considering majoring in buisness");
+        System.out.println("My name is Dhruve");
+        System.out.println("My favorite video game is Valorant");
+        System.out.println("I want to go to UC Berkley");
+        System.out.println("I like playing basketball");
+	}
+}
